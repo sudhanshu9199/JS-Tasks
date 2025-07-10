4f8d1d990c184ec0006c9312b169bf4e2a71ca59
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-07-10 11:39:37 +0530

    Day 33 lecture done with Assignement Basic Pattern problem

---
+++ DSA in JS/Day-33/dsa-33.js
@@ -0,0 +1,96 @@
+// process.stdout.write("*")
+// process.stdout.write("*")
+// process.stdout.write("*")
+// process.stdout.write("*")
+
+
+// let ans = require('prompt-sync')()
+// let n = ans('Enter a number:');
+// for (let i = 1; i <= n; i++) {
+//     for (let j = 1; j <= n; j++) {
+//         process.stdout.write("* ");
+//     }
+//     console.log();
+    
+// }
+
+
+
+
+
+
+/*
+Q 35. Right Angle Traingle
+*
+* *
+* * *
+* * * *
+
+*/
+// let uinput = require('prompt-sync')()
+
+// let ans = uinput('Enter row value: ');
+// for (let i = 1; i <= ans; i++) {
+//     for (let j = 1; j <= i; j++) {
+//         process.stdout.write("* ");
+//     }
+//     console.log();
+    
+// }
+
+
+/* Q 36.
+1
+1 2
+1 2 3 
+1 2 3 4
+1 2 3 4 5 */
+// let uinput = require('prompt-sync')()
+
+// let ans = uinput('Enter row value: ');
+// for (let i = 1; i <= ans; i++) {
+//     for (let j = 1; j <= i; j++) {
+//         process.stdout.write(j+' ');
+//     }
+//     console.log();
+    
+// }
+
+
+
+/* Q 37.
+A
+A B 
+A B C
+A B C D
+A B C D E */
+
+// let uinput = require('prompt-sync')()
+
+// let ans = uinput('Enter row value: ');
+// for (let i = 1; i <= ans; i++) {
+//     for (let j = 1; j <= i; j++) {
+//         process.stdout.write(String.fromCharCode(65 + (j - 1) + " "));
+//     }
+//     console.log();
+    
+// }
+
+
+
+/* Q 38. Inverted Right Angle Triangle
+* * * * *
+* * * *
+* * *
+* *
+*   */
+
+// let uinput = require('prompt-sync')()
+// let ans = uinput('Enter row value: ');
+// for (let i = 1; i <= ans; i++) {
+//     for (let j =1; j <= ans -i + 1; j++) {
+//         process.stdout.write("* ");
+//     }
+//     console.log();
+    
+// }
