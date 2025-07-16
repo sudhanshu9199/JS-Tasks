9f356928f6d84861dd81591eca3666df9ff716ba
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-07-16 10:26:55 +0530

    Dsa-40 & 41 bubble sort, selection sort, insertion sort

---
+++ DSA in JS/Day-40/dsa-41.js
@@ -0,0 +1,47 @@
+/* Bubble Sort */
+// let arr = [1, 4, 3, 2, 9, 10];
+// let n = arr.length;
+// for(let i = 0; i < n - 1; i++) {
+//     for (let j = 0; j < n - i - 1; j++) {
+//         if (arr[j] > arr[j + 1]) {
+//         let temp = arr[j];
+//         arr[j] = arr[j + 1];
+//         arr[j + 1] = temp;
+//     }
+//     }
+// }
+// console.log(arr);
+
+
+/** Selection sort */
+
+// let arr = [1, 4, 3, 2, 9, 10];
+// for (let i = 0; i < arr.length - 1; i++) {
+//     let small = i;
+//     for (let j = i+ 1; j < arr.length; j++) {
+//         if (arr[small] > arr[j]) {
+//             small = j;
+//         }
+//     }
+//     if (i !== small) {
+//         let temp = arr[i];
+//         arr[i] = arr[small];
+//         arr[small] = temp;
+//     }
+// }
+// console.log(arr);
+
+
+/* Insertion sort */
+
+let arr = [1, 4, 3, 9, 2, 10];
+for (let i = 1; i < arr.length; i++) {
+    let key = arr[i];
+    let j = i - 1;
+    while (j >= 0 && arr[j] > key) {
+        arr[j + 1] = arr[j];
+        j--;
+    }
+    arr[j + 1] = key;
+}
+console.log(arr);
