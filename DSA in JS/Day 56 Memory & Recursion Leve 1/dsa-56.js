87def2d712f200d33b0c2c1d4d2c0246022a8ff1
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-08-05 13:22:19 +0530

    DAY 56 started learn memory, function, recursion with few recursion questions

--- DSA in JS/Day 56 Memory & Recursion Leve 1/dsa-56.js
+++ DSA in JS/Day 56 Memory & Recursion Leve 1/dsa-56.js
@@ -1,13 +1,51 @@
-greet();
-function greet () {
-    console.log('Good Night!');
-    Kuch();
-}
+// greet();
+// function greet () {
+//     console.log('Good Night!');
+//     Kuch();
+// }
 
-function Kuch () {
-    console.log('More about,');
-    joke();
-}
-function joke () {
-    console.log('Women are queen.');
-}
+// function Kuch () {
+//     console.log('More about,');
+//     joke();
+// }
+// function joke () {
+//     console.log('Women are queen.');
+// }
+
+/* return type -> void, number, string, etc. */
+
+// function temp (a) {
+//     if (a === 0) return 
+//     console.log('mai tho chal rha hu');
+// }
+// temp (1);
+
+
+/* learn */
+// function temp (a) {
+//     if (a === 0) return 'tap';
+//     console.log('manat raj');
+//     return ''; // this return is mendetry and good practice
+// }
+
+
+
+// intermediate DSA started: Recursion
+// let n = 5;
+// temp (n);
+// function temp (a) {
+//     if (a === 0) return;
+//     console.log('Hello wordl');
+//     temp (a - 1);
+// }
+
+
+/* Q. Print N to 1 using Recursion */
+
+// let n = 10;
+// recurse(n);
+// function recurse(a){
+//     if (a === 0) return;
+//     console.log(a);
+//     recurse(--a);
+// }
