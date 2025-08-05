02ca3c7e525d299a4ec97cd549adcc8e89db5d03
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-08-05 20:02:42 +0530

    Day 57 OOPs level 1 old and new way to create class done

--- Day 57 58 59 OOPs/day-57.js
+++ Day 57 58 59 OOPs/day-57.js
@@ -4,10 +4,10 @@
 // hum ek class bnaayege us class se kai saare objects banege ab wo har object kuchh data hold karega and kuchh funcinalities bhi hold karega jisko hum uske methods kahta hai, ye sab manually naa karna pade ki hum oops ke arte hai follow karta ha
 
 function Ghar() {
-    this.shahar = 'Delhi';
-    this.kamre = 4;
-    this.bathroom = 2;
-    this.kitchen = 1;
+  this.shahar = "Delhi";
+  this.kamre = 4;
+  this.bathroom = 2;
+  this.kitchen = 1;
 }
 let ghar1 = new Ghar();
 
@@ -16,16 +16,46 @@ let ghar1 = new Ghar();
 
 // purana tareeka objects banaane ka ek constructor function se jab javascript mein class ka concept nahi tha
 
+// function Toffee(flavor, price, expire) {
+//   this.flavor = flavor;
+//   this.price = price;
+//   this.expire = expire;
+// }
+// let t1 = new Toffee("vanilla", 10, "23-05-2026");
+// let t2 = new Toffee("Gold", 50, "23-02-2025");
+// let t3 = new Toffee("Dark", 10, "23-05-2026");
 
-function Toffee(flavor, price, expire) {
-    this.flavor = flavor;
-    this.price = price;
-    this.expire = expire;
-}
-let t1 = new Toffee('vanilla', 10, '23-05-2026')
-let t2 = new Toffee('Gold', 50, '23-02-2025')
-let t3 = new Toffee('Dark', 10, '23-05-2026')
 
 
 // Now class is 'New' way in market.
 // har class banaate hi pahli cheej jo banaate hai uski aadat daal lo uska naam hai constructor
+
+// class Toffee {
+//   constructor() {
+//     this.flavor = "vanilla";
+//     this.color = "brown";
+//     this.taste = "sweet";
+//     this.texture = "hard";
+//     this.shape = "cube";
+//     this.size = "small";
+//     this.smell = "sweet";
+//     this.sound = "crunch";
+//     this.temperature = "room temperature";
+//     this.origin = "USA";
+//     this.ingredients = ["sugar", "butter", "milk", "vanilla extract"];
+//   }
+// }
+// let t1 = new Toffee();
+
+
+
+
+class Toffee {
+  constructor(flavor, color, taste) {
+    this.flavor = flavor;
+    this.color = color;
+    this.taste = taste;
+  }
+}
+let t1 = new Toffee('vanilla', 'whiteSmoke', 'sweet');
+let t2 = new Toffee('dark', 'black', 'sweet');
