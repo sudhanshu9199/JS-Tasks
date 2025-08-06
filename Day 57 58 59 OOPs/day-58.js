436efc264bfd841e7e1f3481bbd64e1a6401a904
SUDHANSHU GHOSH <shudhanshukumar9713@gmail.com>
2025-08-06 10:57:40 +0530

    Day 58 start with learn use of '.prototype'

--- Day 57 58 59 OOPs/day-58.js
+++ Day 57 58 59 OOPs/day-58.js
@@ -30,3 +30,27 @@ Has a method `changePassword(oldPass, newPass)` to update password */
 // const u1 = new User('Sudhanshu', 123);
 // u1.changePassword(123, 23)
 // console.log(u1.checkPassword(23));
+
+
+
+/**lecture day 58 */
+
+/* prototype */
+// jab bhi constructor banaaoge aur kuchh aisa mil gaya joh ki sabmein same hai to usey kabhi bhi direct constructor mein banana galat tareeka hota hai, par sahi tareeka hai prototype ka use karna kyuki prototype mein save karne par jitne bhi new ke saath instance banaaoge wo saare instance by default tumhaari proto ki value ko hold karege
+
+// function Toffee(name) {
+//     this.name = name;
+// }
+// Toffee.prototype.price = 3;
+// let t1 = new Toffee('alpenliebe');
+// let t2 = new Toffee('kachha aam');
+
+
+// sabse pahle property ko aapke object mein dhundhege nahi mila to prototype mein dhundte hai
+
+function Toffee(name) {
+    this.name = name;
+    this.price = 34;
+}
+Toffee.prototype.price = 10;
+let t1 = new Toffee('Kaccha aam')
